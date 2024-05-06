249f296bedd74d8b48e233060997ca318cf231b5
Yeoh Xuan <yorkxuan7@gmail.com>
2024-05-06 22:23:18 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ export default function App() {
   function generateNewDice() {
     return {
       id: nanoid(),
-      value: Math.ceil(Math.random() * 3),
+      value: Math.ceil(Math.random() * 6),
       isHeld: false,
     };
   }
